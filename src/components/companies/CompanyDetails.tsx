20ca0f69338a6ed919221a7a16b9bc7bdabbc92d
Rearles <53582049+Rearles@users.noreply.github.com>
2021-11-03 11:48:23 -0600

    HTML basics

--- src/components/companies/CompanyDetails.tsx
+++ src/components/companies/CompanyDetails.tsx
@@ -3,7 +3,14 @@ import React from "react";
 interface Props {}
 
 const CompanyDetails = (props: Props) => {
-  return <div></div>;
+  return <div class="card" style="width: 18rem;">
+    <Image src="..." className="card-img-top" alt="..."/>
+    <div class ="card-body">
+    <h5 class ="card-title">Card title</h5>
+    <p class ="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class ="btn btn-primary">Go somewhere</a>
+    </div>
+  </div>;
 };
 
 export default CompanyDetails;
