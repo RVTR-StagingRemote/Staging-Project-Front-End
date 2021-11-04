86baed99a6f7822e74c5b8f867163855a53d6288
Rearles <53582049+Rearles@users.noreply.github.com>
2021-11-04 09:47:27 -0600

    Start of company details

--- src/components/companies/CompanyDetails.tsx
+++ src/components/companies/CompanyDetails.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 
-interface Props {}
+interface Props {
+  companyName: string,
+  companyDescription: string,
+
+}
 
 const CompanyDetails = (props: Props) => {
   return <div class="card" style="width: 18rem;">
     <Image src="..." className="card-img-top" alt="..."/>
-    <div class ="card-body">
-    <h5 class ="card-title">Card title</h5>
-    <p class ="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <div className ="card-body">
+    <h5 className ="card-title">Card title</h5>
+    <p className ="card-text">{companyName => props.companyName}</p>
     <a href="#" class ="btn btn-primary">Go somewhere</a>
     </div>
   </div>;
