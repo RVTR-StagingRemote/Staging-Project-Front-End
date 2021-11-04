dc2748eec8eae9d5b31ad1a01d16271e9e490359
Rearles <53582049+Rearles@users.noreply.github.com>
2021-11-04 10:29:14 -0600

    Added JSX comments

--- src/components/companies/CompanyDetails.tsx
+++ src/components/companies/CompanyDetails.tsx
@@ -1,20 +1,20 @@
 import React from "react";
 
 interface Props {
-  companyName: string,
-  companyDescription: string,
+    companyName: string,
+    companyDescription: string,
 
 }
-
+{/* This takes a list of props that is defined above and returns a card displaying company details*/ }
 const CompanyDetails = (props: Props) => {
-  return <div class="card" style="width: 18rem;">
-    <Image src="..." className="card-img-top" alt="..."/>
-    <div className ="card-body">
-    <h5 className ="card-title">Card title</h5>
-    <p className ="card-text">{companyName => props.companyName}</p>
-    <a href="#" class ="btn btn-primary">Go somewhere</a>
-    </div>
-  </div>;
+    return <div class="card" style="width: 18rem;">  {/* mess around with this */}
+        <Image src="..." className="card-img-top" alt="..." />  {/* TODO: find image, figure out where to put image in this frontend and link to image in this line */}
+        <div className="card-body">
+            <h5 className="card-title">{props.companyName}</h5> {/* Company Name within the card text that's included in Props */}
+            <p className="card-text">{props.companyDescription}</p> {/* Company Description within the card text that's included in Props */}
+            <a href="#" className="btn btn-primary">Details</a> {/* Details button */}
+        </div>
+    </div>;
 };
 
 export default CompanyDetails;
