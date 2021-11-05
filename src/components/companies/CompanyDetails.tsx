3c001312c17583dcdb59db9cf7e8e89473884c2a
Rearles <53582049+Rearles@users.noreply.github.com>
2021-11-05 12:28:15 -0600

    npm update and update company details

--- src/components/companies/CompanyDetails.tsx
+++ src/components/companies/CompanyDetails.tsx
@@ -11,7 +11,7 @@ const CompanyDetails = (props: Props) => {
     return <div className="card my-2 ps-0 pe-4">
         <div className="row">
             <div className="col-md-4">  {/* mess around with this */}
-                <Image width={400} height={300} src="https://images.unsplash.com/photo-1495314736024-fa5e4b37b979?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1173&q=80" className="card-img-top" className="img-fluid rounded-start" />  {/* TODO: find image, figure out where to put image in this frontend and link to image in this line */}
+                <image width={400} height={300} src={require("https://images.unsplash.com/photo-1495314736024-fa5e4b37b979?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1173&q=80")} className="card-img-top" />  {/* TODO: find image, figure out where to put image in this frontend and link to image in this line */}
             </div>
             <div className="col-md-6">
                 <div className="card-body">
