b6bc1f9e15937f88acaa7ba29718b9c4079585d3
Jonathan Bukowsky <jonathan.bukowsky@revature.net>
2021-11-08 15:58:14 -0500

    Remove global style, update comments

--- src/components/occupations/Occupations.tsx
+++ src/components/occupations/Occupations.tsx
@@ -2,31 +2,41 @@ import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 
+// Props for Occupations component
 interface Props {
+    // title to display on card
     title: string;
+    // description to display on card
     description: string;
+    // image to display on card
+    imageURI: string;
 }
 
 // TODO:
-// This component will need correct URIs for the buttons. I'm expecting to get that from props.
+// This component will need correct routes for the buttons. I'm currently expecting to get that from props.
 // There is an issue with the image sizing/layout of the card. I will need to look into the image size + css to try to remedy this
-// This component will need a URI for the image. I'm expecting to get that (or the image component itself) from props
 // Should the buttons have the right arrow?
-// I'm thinking of creating a .scss for the styles here and using that instead of bootstrap classes to style this component.
+// I'm thinking of creating a .scss for the styles here and using that instead of or along with bootstrap classes to style this component.
 
+///
+/// Occupation component
+/// displays a card to show an occupation's information. meant to be used as part of a list of occupations.
 const Occupations = (props: Props) => {
     return (
         <div className="card my-2 ps-0 pe-4">
             <div className="row">
+                {/* image */}
                 <div className="col-md-4">
                     <Image
                         width={400}
                         height={300}
-                        src="https://images.unsplash.com/photo-1581092160562-40aa08e78837?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2370&q=80"
+                        //src="https://images.unsplash.com/photo-1581092160562-40aa08e78837?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2370&q=80"
+                        src={props.imageURI}
                         className="img-fluid rounded-start"
                         alt="https://via.placeholder.com/300x200?text=Course+3"
                     />
                 </div>
+                {/* card body */}
                 <div className="col-md-6">
                     <div className="card-body">
                         <h5 className="card-title">{props.title /* Engineers */}</h5>
@@ -42,6 +52,7 @@ const Occupations = (props: Props) => {
                         </p>
                     </div>
                 </div>
+                {/* navigation buttons */}
                 <div className="col-md-2 d-flex flex-column justify-content-center">
                     <Link href="#">
                         <a className="btn rvtr-btn-primary my-3">
