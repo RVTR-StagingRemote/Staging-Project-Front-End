f4657859287daa758052678296d26ccdeb122eb8
YYarytsk <iamfromukraine@gmail.com>
2021-11-09 11:31:09 -0500

    added a few comments

--- src/components/occupations/OccupationDetails.tsx
+++ src/components/occupations/OccupationDetails.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 interface Props {
-  occupationTitle: string;
-  occupationDescription: string;
-  occupationRequirements: string;
+  occupationTitle: string; // Occupation Title to be displayed according to selection from occupations componenet 
+  occupationDescription: string; // Occupation Description to be displayed according to selection from occupations componenet
+  occupationRequirements: string; // Occupation Requirements to be displayed according to selection from occupations componenet
 }
 
 const OccupationDetails = (props: Props) => {
@@ -16,6 +16,7 @@ const OccupationDetails = (props: Props) => {
             <Image
               width={400}
               height={300}
+              // image to be displayed according to occupation selected
               src={require("https://images.unsplash.com/photo-1556761175-5973dc0f32e7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2832&q=80")}
               className="img-fluid rounded-start"
               alt="https://via.placeholder.com/300x200?text=Course+3"
@@ -47,6 +48,7 @@ const OccupationDetails = (props: Props) => {
             </div>
           </div>
         </div>
+        {/* loged in navigation buttons */}
         <div className="col-md-2 d-flex flex-column justify-content-rihgt">
           <a href="#" className="btn btn-primary">
             Back
