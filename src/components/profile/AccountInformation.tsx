0d3aa19dc83e579e05c4575ea39c03b1ce395f36
Gabriel Gil <71419038+gabrielgil245@users.noreply.github.com>
2021-11-09 15:09:40 -0600

    Revert "Occupation details   YuriyYarytskyy"

--- src/components/profile/AccountInformation.tsx
+++ src/components/profile/AccountInformation.tsx
@@ -2,18 +2,6 @@ import React from "react";
 
 interface Props {}
 
-/**
- *
- * @param props first name
- * last name
- * phone #
- * street address
- * city
- * state
- * zipcode
- * date of birth
- * @returns
- */
 export const AccountInformation = (props: Props) => {
   return <div></div>;
 };
